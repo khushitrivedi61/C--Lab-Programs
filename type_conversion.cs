83707ab93ae8067d8c34ebad35e8efc3c356c0eb
Khushi Trivedi <khushi.trivedi129697@marwadiuniversity.ac.in>
2026-02-13 11:19:25 +0530

    Add files via upload

---
+++ type_conversion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1 = 10;
+        float num2 = 5.5f;
+
+        float automaticConversion = num1;
+
+        int forcedConversion = (int)num2;
+
+        Console.WriteLine("Integer value: " + num1);
+        Console.WriteLine("Float value: " + num2);
+        Console.WriteLine("After automatic conversion (int to float): " + automaticConversion);
+        Console.WriteLine("After forced conversion (float to int): " + forcedConversion);
+    }
+}
